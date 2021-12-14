501ca7a16dcbac342c6b920366d8149e7c6e5224
Fernando Serra <fernando.serra.r@gmail.com>
2021-12-13 23:44:06 -0300

    Add create Phones

--- api/controllers/PhonesController.js
+++ api/controllers/PhonesController.js
@@ -5,7 +5,21 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const Phones = require("../models/Phones");
+
 module.exports = {
   
+    async create(req, res) {
+        console.log("Ejecutando create")
+        try {
+            let params = req.allParams();
+            const phone = await Phones.create({
+                phone: params.phone,
+            }).fetch();
+            return res.ok(phone);
+        } catch (error) {
+            return res.serverError(error)
+        }
+    },
 
 };
